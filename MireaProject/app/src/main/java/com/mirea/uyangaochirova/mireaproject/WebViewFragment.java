f5ce759af4abb71829ad4468b0ff240bea1428c9
UyangaOchirova <uyanga011003@gmail.com>
2024-03-30 14:08:53 +0300

    MireaProject modified

--- MireaProject/app/src/main/java/com/mirea/uyangaochirova/mireaproject/WebViewFragment.java
+++ MireaProject/app/src/main/java/com/mirea/uyangaochirova/mireaproject/WebViewFragment.java
@@ -2,11 +2,13 @@ package com.mirea.uyangaochirova.mireaproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +62,12 @@ public class WebViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_web_view, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        WebView webView = view.findViewById(R.id.webView);
+        webView.loadUrl("https://www.tutu.ru/geo/rossiya/kurort/baikal/");
     }
 }
---
+++ Practice4/musicplayer/src/main/java/com/mirea/uyangaochirova/musicplayer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mirea.uyangaochirova.musicplayer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mirea.uyangaochirova.musicplayer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
